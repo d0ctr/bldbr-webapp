5b4159fd64a76480d03c6f081127605a49bc35e5
Danila Vasilev <mr.den.vi@gmail.com>
2024-04-28 21:22:18 +0000

    crumbs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
-        <Script src="https://telegram.org/js/telegram-web-app.js"></Script>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy='beforeInteractive'></Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
