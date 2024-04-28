c2f9947359782fc6979adb0d02fee0c706a7da5b
Danila Vasilev <mr.den.vi@gmail.com>
2024-04-28 20:07:07 +0000

    crumbs

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,7 +1,7 @@
 import type { Game } from '@/api/getGames';
 import Share from '@/icons/Share';
 import getTelegram, { switchInlineQuery } from '@/utils/telegram';
-import { Card, CardBody, CardFooter, CardHeader, Image } from '@nextui-org/react';
+import { Card, CardBody, CardFooter, CardHeader, Divider, Image } from '@nextui-org/react';
 
 export default function ResultCard({ title, image_url, text, callback }: Game & { callback: string }) {
     async function processRedirect(formData: FormData) {
@@ -14,14 +14,15 @@ export default function ResultCard({ title, image_url, text, callback }: Game & 
             {image_url
                 ?
                 <CardHeader className='flex-col w-full p-0'>
-                    <Image radius='md' src={image_url} alt={title} width={500} height={200} className='z-0 select-none object-cover'/>
+                    <Image src={image_url} alt={title} width={500} height={200} className='z-0 select-none object-cover'/>
                     <div className='z-10 flex flex-col justify-end w-full max-w-[500px] bg-gradient-to-t from-black to-transparent text-center -mt-36 h-36'>
-                        <div className='pb-4 px-2 font-bold text-white'>{title}</div>
+                        <h1 className='pb-4 px-2 font-bold text-large text-white'>{title}</h1>
                     </div>
                 </CardHeader>
                 :
-                <CardHeader className='flex flex-row justify-center text-center'>
-                    <h1 className='w-full max-w-[500px] font-bold'>{title}</h1>
+                <CardHeader className='flex flex-col justify-center text-center pb-0'>
+                    <h1 className='w-full max-w-[500px] font-bold text-large pb-2'>{title}</h1>
+                    <Divider />
                 </CardHeader>
             }
             <CardBody>
