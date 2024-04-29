f2f59c7a48828a3f7d36f2e7f46104133d005d55
Danila Vasilev <mr.den.vi@gmail.com>
2024-04-29 18:32:18 +0200

    crumbs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { getResults } from '@/api/getGames';
 import ResultCard from '@/components/ResultCard';
 import PaperPlane from '@/icons/PaperPlane';
-import { Button, Divider } from '@nextui-org/react';
+import { Button, Divider, Select, SelectItem } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 import type { Game } from '@/api/getGames';
 import { useFormStatus } from 'react-dom';
@@ -64,15 +64,15 @@ export default function Home() {
 
 
   return (
-    <main className={(isDark ? 'dark ' : '') + 'flex flex-row justify-center min-h-screen min-w-80 bg-background text-foregound'}>
+    <main className={(isDark ? 'dark ' : 'dark ') + 'flex flex-row justify-center min-h-screen min-w-80 bg-background text-foregound'}>
       <div className='flex flex-col justify-start items-center gap-4 max-w-lg py-4'>
         <form className='flex flex-row gap-2 h-12 px-2 min-w-full' action={handleForm}>
-          <select className='flex-none pl-2 text-3xl w-12 rounded-md outline outline-2 outline-offset-1 outline-transparent hover:outline-blue-500 bg-default hover:bg-default-100' style={{appearance: 'none'}} defaultValue={category || 'game'} name='category'>
-              {
-                categories.map((cat) => <option key={cat.value} value={cat.value}>{cat.label}</option>)
-              }
-          </select>
-          <input className='flex-1 px-2 min-w-0 w-full h-full text-large outline outline-2 outline-offset-1 outline-transparent hover:outline-blue-500 bg-default hover:bg-default-100 text-foreground' type='text' name='query' style={{appearance: 'none'}} required/>
+          <Select className='flex-none w-12' radius='sm' classNames={{mainWrapper: 'h-full w-fit', selectorIcon: 'hidden', trigger: 'h-full', innerWrapper: 'w-full h-full', value: 'text-center text-2xl'}} selectorIcon={<></>} selectedKeys={['game']} aria-label='Category'>
+            { categories.map(cat => (
+              <SelectItem key={cat.value} value={cat.value}>{cat.label}</SelectItem>
+            )) }
+          </Select>
+          <input className='flex-1 px-2 min-w-0 w-full h-full rounded-md text-large outline outline-2 outline-offset-1 outline-transparent hover:outline-blue-500 bg-default hover:bg-default-100 text-foreground' type='text' name='query' style={{appearance: 'none'}} required/>
           <SubmitData className='flex-none h-full w-12 pr-0.5 rounded-md disabled:outline-none outline outline-2 outline-offset-1 outline-transparent hover:outline-blue-500 enabled:hover:text-blue-500 enabled:bg-default enabled:hover:bg-default-100' hasFailed={hasFailed}/>
         </form>
         <Divider className='w-screen overflow-visible'/>
